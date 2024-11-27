a8a252cfd5a41abe84c31b916e60b7f937d64010
Bruno Holanda <bssah@cesar.school>
2024-11-27 19:18:36 -0300

    novo comit

--- javascript.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("change-bg-btn");
-    const colors = ["#FFC0CB", "#FFD700", "#90EE90", "#87CEFA", "#FFA07A"];
-
-    button.addEventListener("click", () => {
-        const randomColor = colors[Math.floor(Math.random() * colors.length)];
-        document.body.style.backgroundColor = randomColor;
-    });
-});
